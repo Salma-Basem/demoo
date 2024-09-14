75a2ea1fcfc03964383841af235501d78e7914da
salma basem <20191743@fue.edu.eg>
2024-09-14 16:33:40 +0300

    Update Home Page Videos

--- src/app/Components/Home/home.component.ts
+++ src/app/Components/Home/home.component.ts
@@ -2,7 +2,10 @@ import { Component, ElementRef, HostBinding, HostListener, Renderer2 } from '@an
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { LanguageService } from 'src/app/Services/language.service';
+import { VideoControlService } from 'src/app/Services/VideoControl.service';
+
 declare var bootstrap: any; 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -35,41 +38,45 @@ export class HomeComponent {
     },
     nav: false
   }
-
-
-
-  constructor(
-    private languageService: LanguageService,
-    private sanitizer: DomSanitizer,
-    private el: ElementRef, private renderer: Renderer2
-  ) { }
+  private player: any;
+  private isPlayerReady = false;
 
   language: string = 'en';
-
+  isLoading: boolean = false;
+  showScrollToTopButton = false;
+  private iframe: HTMLIFrameElement | null = null;
+  
   @HostBinding('attr.dir') get dir() {
     return this.language === 'ar' ? 'rtl' : 'ltr';
   }
 
-
+  constructor(
+    private languageService: LanguageService,
+    private sanitizer: DomSanitizer,
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private videoControlService: VideoControlService
+  ) { }
 
   ngOnInit() {
-    this.isLoading=true;
+    this.isLoading = true;
     // Subscribe to language changes
     this.languageService.getLanguage().subscribe(language => {
-      this.isLoading=false;
+      this.isLoading = false;
       this.language = language;
-     
     });
 
-  
+    // Intersection Observer to detect when iframe is in view
+    this.initIntersectionObserver();
+   
   
   }
 
   changeLanguage(newLanguage: string) {
     this.languageService.setLanguage(newLanguage);
   }
+ 
 
-  showScrollToTopButton = false;
 
   @HostListener('window:scroll', ['$event'])
   onScroll() {
@@ -90,5 +97,25 @@ export class HomeComponent {
     this.languageService.setLanguage(language);
   }
 
-  isLoading:boolean=false
+  private initIntersectionObserver() {
+    const iframe = this.el.nativeElement.querySelector('#videoIframe');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Modify iframe src to include autoplay
+          iframe.src += '&autoplay=1';
+          // Optionally unobserve once the video has started
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      rootMargin: '0px',
+      threshold: 0.5 // Adjust this value as needed
+    });
+
+    // Start observing the iframe
+    observer.observe(iframe);
+  }
+  
+
 }
