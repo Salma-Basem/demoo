5a2caaa54fb6aa76b7afa481fc00d1424f7817e1
salma basem <20191743@fue.edu.eg>
2024-09-15 20:51:06 +0300

    update shortstory page

--- src/app/Components/PublishingHouse/publishing-house.component.ts
+++ src/app/Components/PublishingHouse/publishing-house.component.ts
@@ -8,7 +8,7 @@ import { LanguageService } from 'src/app/Services/language.service';
 })
 export class PublishingHouseComponent {
   language: string = 'en';
-
+  isArabic: boolean = false;
   @HostBinding('attr.dir') get dir() {
     return this.language === 'ar' ? 'rtl' : 'ltr';
   }
@@ -19,6 +19,8 @@ export class PublishingHouseComponent {
     // Subscribe to language changes
     this.languageService.getLanguage().subscribe(language => {
       this.language = language;
+      this.isArabic = this.language === 'ar';
+
     });
   }
     // Switching Languages 
