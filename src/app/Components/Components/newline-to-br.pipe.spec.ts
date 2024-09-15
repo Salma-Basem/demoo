f849c2ad544d67aedab277e52ccb3d8246a3d607
salma basem <20191743@fue.edu.eg>
2024-09-15 18:00:20 +0300

    update pages:

--- src/app/Components/Components/newline-to-br.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NewlineToBrPipe } from './newline-to-br.pipe';
-
-describe('NewlineToBrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NewlineToBrPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
